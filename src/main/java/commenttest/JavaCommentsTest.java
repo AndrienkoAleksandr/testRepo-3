cec05be9d215057e5541a9bd139acdcc87c645e2
logarifm6@gmail.com <iedexmain@gmail.com>
2017-03-21 23:33:28 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1490027601644
+//first_change1490135623821
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
