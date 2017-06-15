642b1a8b6fb10950be087c21a7188854b724a19e
AndrienkoAleksandr <useruyth24@some.mail>
2017-06-15 19:45:46 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1495735096957
+//first_change1497553540928
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
