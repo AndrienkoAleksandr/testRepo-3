9d38dff21da79a990612443323b1f86670c439f6
AndrienkoAleksandr <che@eclipse.org>
2018-01-10 00:09:27 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1508710693193
+//first_change1515540691614
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
